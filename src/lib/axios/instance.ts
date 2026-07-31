eb63b6cc1c5069699553dd4518a29e809f108d4b
N0RRY-20 <orywu1994@gmail.com>
2026-07-31 14:17:46 +0700

    feat: add button component and page head for improved UI structure
    - Introduced a new Button component using class-variance-authority for styling variants.
    - Created a PageHead component for managing document head elements like title and meta tags.
    - Updated the index page to utilize the new Button and PageHead components.
    - Added environment configuration for API URL.
    - Implemented a custom Axios instance with request and response interceptors for handling authentication.
    - Enhanced global styles with new color variables and Tailwind CSS imports.
    - Included new dependencies in package.json for improved styling and functionality.

---
+++ src/lib/axios/instance.ts
@@ -0,0 +1,48 @@
+import { environment } from "@/configs/environment";
+import axios from "axios";
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
+
+interface CustomSession extends Session {
+  accessToken?: string;
+}
+
+const headers = { "content-type": "apolication/json" };
+
+const instance = axios.create({
+  baseURL: environment.API_URL,
+  timeout: 5000,
+  headers,
+});
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  async function (config) {
+    // Do something before request is sent
+    const session: CustomSession | null = await getSession();
+
+    if (session && session.accessToken) {
+      config.headers.Authorization = `Bearer ${session.accessToken}`;
+    }
+
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  },
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  },
+);
